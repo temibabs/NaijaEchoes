a26f64c0228a1289dcabad8141ec710dc756ff03
temi <temi.ayo.babs@gmail.com>
2023-09-09 07:01:55 +0100

    Support for Streamlit deploy

--- utils/constants.py
+++ utils/constants.py
@@ -3,26 +3,21 @@ import io
 DEFAULT_MESSAGE = """Hi there! I am"""
 
 
-def read_image(file_name: str):
-    with open(f"/mount/src/naijaechoes/avatars/{file_name}", mode="rb") as f:  # supported for Streamlit deployment
-        return io.BytesIO(f.read())
-
-
 CHARACTER_AVATARS = {
-    "Bola Ahmed Tinubu": read_image(file_name="Bola Tinubu.jpeg"),
-    "Buchi Emecheta": read_image(file_name="Buchi Emecheta.webp"),
-    "Burna Boy": read_image(file_name="Burna Boy.jpeg"),
-    "Chimamanda Adichie": read_image(file_name="Chimamanda.jpeg"),
-    "Chinua Achebe": read_image(file_name="Chinua Achebe.jpeg"),
-    "Davido": read_image(file_name="Davido.jpeg"),
-    "Don Jazzy": read_image(file_name="Don Jazzy.jpeg"),
-    "Ezra Olubi": read_image(file_name="Ezra Olubi.jpeg"),
-    "Iyin Aboyeji": read_image(file_name="Iyin Aboyeji.jpeg"),
-    "Odun Eweniyi": read_image(file_name="Odun Eweniyi.jpeg"),
-    "Peter Obi": read_image(file_name="Peter Obi.webp"),
-    "Wizkid": read_image(file_name="Wizkid.webp"),
+    "Bola Ahmed Tinubu": "https://media.premiumtimesng.com/wp-content/files/2023/07/President-Bola-Ahmed-Tinubu.jpg",
+    "Buchi Emecheta": "https://cdn.britannica.com/86/234886-050-7AE740FF/Author-Buchi-Emecheta.jpg?w=400&h=300&c=crop",
+    "Burna Boy": "https://yt3.googleusercontent.com/P6SjTt-BXjTWeUcpBX-dDUcLhpCboucwr6PN0cpvR3Z7BNq7V0-CmUbVPjGbWyB33ZaO9APa=s900-c-k-c0x00ffffff-no-rj",
+    "Chimamanda Adichie": "https://www.chimamanda.com/wp-content/uploads/2021/10/CNA-01.jpg",
+    "Chinua Achebe": "https://www.theparisreview.org/il/e4b8f842fe/large/Chinua-Achebe.jpg",
+    "Davido": "https://leadership.ng/wp-content/uploads/2023/08/davido-4.png",
+    "Don Jazzy": "https://guardian.ng/wp-content/uploads/2022/05/Don-Jazzy.jpg",
+    "Ezra Olubi": "https://pbs.twimg.com/profile_images/1327966865155624960/4w__BVsB_400x400.jpg",
+    "Iyin Aboyeji": "https://www.billionairetomorrow.com/wp-content/uploads/2021/08/Header-E-.png",
+    "Odun Eweniyi": "https://guardian.ng/wp-content/uploads/2021/01/AXR_2919-scaled.jpg",
+    "Peter Obi": "https://leadership.ng/wp-content/uploads/2023/04/Obi.png",
+    "Wizkid": "https://leadership.ng/wp-content/uploads/2022/10/Wizkid-1.webp",
     "Wole Soyinka": "https://images4.penguinrandomhouse.com/author/29176",
-    "Yele Bademosi": read_image(file_name="Yele Bademosi.jpeg")
+    "Yele Bademosi": "https://blog.obiex.finance/content/images/2021/12/YELE-B.jpg"
 }
 
 CHARACTERS = CHARACTER_AVATARS.keys()
